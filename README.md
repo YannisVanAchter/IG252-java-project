2109f3035d9f2107b2bbaf53ecc01156c5051476
Yannis Van Achter <91072987+YannisVanAchter@users.noreply.github.com>
2026-04-26 18:00:05 +0200

    Add files necessary for containerization
    Add containerization to the project:
    - new docker-compose.yml file
    - application and database Dockerfiles (src/Dockerfile, database/Dockerfile)
    - .env.test template.
    
    Update the README file with instructions on using the provided .env.test file and the procedure for creating and running containers. Ignore the IDE directories and the .env file in .gitignore.
    
    The build process for java:
    Main.java in the image and runs Xvfb/x11vnc + noVNC so the application is accessible via the exposed VNC web port. Also add a simple Swing UI for testing and comments to src/Main.java.

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # IG252-java-project
+
+## Lancer le projet
+
+Pour lance le projet, assurez-vous d'être en possession de l'application Docker pour gérer la conteneurisation.
+
+### Gestion des variables d'environnement
+
+Vous trouverez les variables d'environnement dans le fichier [.env.test](./.env.test). 
+
+Copiez le contenu du fichier dans un nouveau fichier intitulé `.env` à la racine du projet. 
+
+Une fois fait, faites attention a changer les valeurs des variables `MYSQL_ROOT_PASSWORD`, `MYSQL_PASSWORD` et `TZ` en fonction de votre situation peronnelle.
+
+### Build and start container
+
+```powershell
+IG252-java-project>docker-compose up --build
+```
+
+### Accèder au projet lancé
+
+Aller à l'adresse suivante une fois lancé: [http://localhost:6901/vnc.html](http://localhost:6901/vnc.html)
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
+import javax.swing.JOptionPane;
+
 public class Main {
+
+    // TODO: Remove Swing from file, it was there for tests purposes while contenerization. 
+    /**
+     * Main function which launch the program. 
+     * 
+     * TODO: clear the method. 
+     * 
+     * @effect create a thread for stocks management. 
+     */
     public static void main(String[] args) {
+        // Test interface
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Magasin du Grand Bazard");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -10,5 +22,8 @@ public class Main {
             frame.setLocationRelativeTo(null);
             frame.setVisible(true);
         });
+
+        // Test input and interaction
+        JOptionPane.showConfirmDialog(null, "Test input");
     }
 }
