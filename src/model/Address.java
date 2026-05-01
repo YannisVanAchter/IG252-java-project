e74b68b6714fe19b06b8196eca8e8307520b03da
Yannis Van Achter <91072987+YannisVanAchter@users.noreply.github.com>
2026-05-01 21:22:22 +0200

    Update Address.java
    Location -> Locality review

--- src/model/Address.java
+++ src/model/Address.java
@@ -3,29 +3,29 @@ package model;
 import exception.DataValidationException;
 
 /**
- * This class represents an address, which consists of a street name, street number, and a location.
+ * This class represents an address, which consists of a street name, street number, and a locality.
  * The class includes validation for the street number to ensure it is a positive integer, \
- * and for the location to ensure it is not null. 
- * The location itself is represented by the Location class.
+ * and for the locality to ensure it is not null. 
+ * The locality itself is represented by the locality class.
  * The Address class provides two constructors: 
- *  1. that takes a Location object directly
- *  2. that takes the components of a Location (name and postal code) to create a Location object.
+ *  1. that takes a locality object directly
+ *  2. that takes the components of a locality (name and postal code) to create a locality object.
  */
 public class Address {
     private String streetName;
     private int streetNumber;
-    private Locality location;
+    private Locality locality;
 
-    public Address(String streetName, int streetNumber, Locality location) throws DataValidationException {
+    public Address(String streetName, int streetNumber, Locality locality) throws DataValidationException {
         setStreetName(streetName);
         setStreetNumber(streetNumber);
-        setLocality(location);
+        setLocality(locality);
     }
 
-    public Address(String streetName, int streetNumber, String locationName, int locationPostalCode) throws DataValidationException {
+    public Address(String streetName, int streetNumber, String localityName, int localityPostalCode) throws DataValidationException {
         setStreetName(streetName);
         setStreetNumber(streetNumber);
-        setLocality(new Locality(locationName, locationPostalCode));
+        setLocality(new Locality(localityName, localityPostalCode));
     }
 
     public String getStreetName() { return streetName; }
@@ -44,14 +44,14 @@ public class Address {
         this.streetNumber = streetNumber;
     }
 
-    public Locality getLocality() { return location; }
+    public Locality getLocality() { return locality; }
 
-    private void setLocality(Locality location) throws DataValidationException {
-        if (location == null) {
-            String message = "Location setting error, location is null when it shouldn't";
+    private void setLocality(Locality locality) throws DataValidationException {
+        if (locality == null) {
+            String message = "locality setting error, locality is null when it shouldn't";
             throw new DataValidationException(message);
         }
-        this.location = location;
+        this.locality = locality;
     }
 
     @Override
@@ -60,14 +60,14 @@ public class Address {
         if (obj == null || getClass() != obj.getClass()) return false;
 
         Address other = (Address) obj;
-        return streetName.equals(other.getStreetName()) && streetNumber == other.getStreetNumber() && location.equals(other.getLocality());
+        return streetName.equals(other.getStreetName()) && streetNumber == other.getStreetNumber() && locality.equals(other.getLocality());
     }
 
     @Override
     public int hashCode() {
         int result = streetName.hashCode();
         result = 31 * result + streetNumber;
-        result = 31 * result + location.hashCode();
+        result = 31 * result + locality.hashCode();
         return result;
     }
 }
